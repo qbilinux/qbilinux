4bb16982fea1a4f067c595bc1d0c42c9d5bba553
MATSUKI Toshihisa <5369468+toshi-mtk@users.noreply.github.com>
2017-04-29 17:48:58 +0900

    add pkglist.py.

---
+++ admin/pkglist.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: euc-jp -*-
+
+import os, pickle
+
+basedir = '/home/ftp/pub/Plamo-6.x/'
+archdir = ('x86/', 'x86_64/')
+channel = ('plamo', 'contrib')
+
+'''
+__blockpkgs: updatepkg だけでアップデートできないパッケージは，デフォル
+トでは表示しないようにする．
+ただし get_pkginfo.py で -b オプションを指定すれば，これらも合わせて表
+示される．
+'''
+blockpkgs = ['aaa_base', 'devs', 'etc', 'hdsetup', 'network_configs',
+        'shadow', 'sysvinit']
+
+'''
+__replaces: 改名，分割，集約されたパッケージを追跡するために，旧パッケ
+ージ名を新パッケージ名にマップする．
+ex: 'tamago' -> 'tamago_tsunagi'
+    'python' -> 'Python2', 'Python3' -> 'Python'
+'''
+replace_list = {'tamago': 'tamago_tsunagi', 'python': 'Python2',
+        'Python3': 'Python', 'pycups2': 'py2cups', 'pycurl2': 'py2curl'}
+
+'''
+__no_install: これらのパッケージは updatepkg -f 以外の作業が必要になる
+ので，ダウンロードはできるが自動インストールはしない．
+'''
+no_install = ['grub', 'lilo', 'kernel', 'kernel_headers', 'kernelsrc',
+        'timezone', 'docbook_xml_4.1.2', 'docbook_xml_4.2', 'docbook_xml_4.3',
+        'docbook_xml_4.4', 'docbook_xml_4.5', 'docbook_xml_5.0']
+
+for arch in archdir:
+    allpkgs= {}
+    allpkgs['__blockpkgs'] = blockpkgs
+    allpkgs['__replaces'] = replace_list
+    allpkgs['__no_install'] = no_install
+    for ch in channel:
+        pkg_path = '{}{}{}/'.format(basedir, arch, ch)
+        for root, dirs, files in os.walk(pkg_path):
+            if 'old' in dirs:
+                dirs.remove('old')
+            if 'NG' in dirs:
+                dirs.remove('NG')
+            if '11_mate.old' in dirs:
+                dirs.remove('11_mate.old')
+            for i in files:
+                if '.txz' in i or '.tgz' in i:
+                    print i
+                    (base, vers, p_arch, tmp) = i.split('-')
+                    (build, ext) = tmp.split('.')
+                    path = root.replace(basedir + arch, '')
+                    allpkgs[base] = (vers, p_arch, build, ext, path)
+    with open('{}allpkgs.pickle'.format(basedir + arch), 'w') as f:
+        pickle.dump(allpkgs, f)
