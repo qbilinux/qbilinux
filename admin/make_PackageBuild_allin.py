58c5e38cec02979d27abadc06c9c598ed44e101c
Toshihisa Matsuki <5369468+toshi-mtk@users.noreply.github.com>
2019-03-21 15:36:15 +0900

    change meson option in make_PackageBuild.

--- admin/make_PackageBuild_allin.py
+++ admin/make_PackageBuild_allin.py
@@ -155,7 +155,7 @@ do_config() {
     if [ -f ${S[$1]}/meson.build ] ; then
       export PKG_CONFIG_PATH=/usr/${libdir}/pkgconfig:/usr/share/pkgconfig:/opt/kde/${libdir}/pkgconfig
       export LDFLAGS='-Wl,--as-needed' 
-      meson --prefix=/usr --libdir=/usr/${libdir} --sysconfdir=/etc --localstatedir=/var --mandir=/usr/share/man ${OPT_CONFIG[$1]} ${S[$1]}
+      meson --prefix=%s --libdir=%s/${libdir} --sysconfdir=/etc --localstatedir=/var --mandir=/usr/share/man ${OPT_CONFIG[$1]} ${S[$1]}
     fi
     if [ $? != 0 ]; then
 	echo "configure error. $0 script stop"
